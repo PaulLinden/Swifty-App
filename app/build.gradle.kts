268eb631440c6a4338ab6451fee1b3aefa8c775f
Paul Lindén <linden.91.pl@gmail.com>
2024-01-18 13:09:22 +0100

    Added Log in logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         viewBinding = true
@@ -37,10 +37,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.6.0")
-    implementation("androidx.navigation:navigation-ui:2.6.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("androidx.navigation:navigation-ui:2.7.6")
+    implementation ("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/swifty/MainActivity.java
+++ app/src/main/java/com/example/swifty/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.swifty;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,25 +29,75 @@ public class MainActivity extends AppCompatActivity {
 
         TextView usernameInput = findViewById(R.id.loginUsername);
         TextView passwordInput = findViewById(R.id.loginPassword);
+        Button submit = findViewById(R.id.loginButton);
 
-        boolean isUser = isValidUser(usernameInput.getText().toString(), passwordInput.getText().toString());
+        submit.setOnClickListener(v -> new Thread(() -> {
+            boolean isUser;
 
-        if (isUser){
-            //Navigate to homepage
-        }else {
-            // Create an AlertDialog.Builder
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            // Set the dialog title and message
-            builder.setTitle("Error!")
-                    .setMessage("username or password is not valid.");
-        }
+            try {
+                isUser = isValidUser(usernameInput.getText().toString(), passwordInput.getText().toString());
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            Log.d("MyApp", "IS USER" + isUser);
+            if (isUser) {
+                //Navigate to homepage
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+
+                //pass data to the new activity
+                //intent.putExtra("key", "value");
+
+                // Start the new activity
+                startActivity(intent);
+
+            } else {
+                runOnUiThread(() -> {
+                    // Display an alert dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Error!").setMessage("Username or password is not valid.");
+                    builder.setPositiveButton("OK", (dialog, which) -> {
+                        // You can add code to handle the "OK" button click if needed
+                        dialog.dismiss(); // Dismiss the dialog
+                    });
+                    builder.show(); // Show the dialog
+                });
+            }
+        }).start());
     }
 
-    public boolean isValidUser(String usernameInput, String passwordInput){
-        if(true){
-            return true;
-        }
-        return false;
-    }
+    public boolean isValidUser(String usernameInput, String passwordInput) throws JSONException {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
+        OkHttpClient client = new OkHttpClient();
+        String url = "adress";
+
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("username", usernameInput);
+        jsonBody.put("password", passwordInput);
+
+        RequestBody requestBody = RequestBody.create(JSON, String.valueOf(jsonBody));
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .header("Content-Type", "application/json")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+                String responseBody = response.body().string();
+                JSONObject jsonData = new JSONObject(responseBody);
+
+                // Log JSON data
+                Log.d("MyApp", "JSON data: " + jsonData);
+                return true;
+            }else {
+                return false;
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
